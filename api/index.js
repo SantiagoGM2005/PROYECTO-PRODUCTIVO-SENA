fd7f424b4a64702004332c5f7fe11fd3a4e0ec91
SantiagoGM2005 <santigomelo12@gmail.com>
2026-07-02 11:22:07 +0200

    cargo

--- api/index.js
+++ api/index.js
@@ -20,7 +20,7 @@ const orderRoute = require("./routes/Order");
 app.use(express.json())
 
 app.use(cors({
-    origin: "https://proyecto-productivo-sena-msv3.vercel.app", // La URL exacta de tu frontend
+    origin: "*", // La URL exacta de tu frontend
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
