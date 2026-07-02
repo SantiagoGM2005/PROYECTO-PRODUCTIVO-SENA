de9d9c039fc1ec02c8c8e90953f6f767ce1d2d3b
SantiagoGM2005 <santigomelo12@gmail.com>
2026-07-02 02:32:25 +0200

    cargando de nuevo URL del backend actualizado

--- client/src/Redux/Constants/BASE_URL.js
+++ client/src/Redux/Constants/BASE_URL.js
@@ -1,2 +1,2 @@
 //export const BASE_URL = "http://localhost:3000"
-export const BASE_URL = "https://proyecto-productivo-sena-msv3.vercel.app"
+export const BASE_URL = "https://proyecto-productivo-sena.vercel.app"
