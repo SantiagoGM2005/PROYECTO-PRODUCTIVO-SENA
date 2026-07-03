df1cacc261e13d0e34dee92f849fed9607c8d97e
SantiagoGM2005 <santigomelo12@gmail.com>
2026-07-03 11:21:06 +0200

    cambio Cors

--- api/index.js
+++ api/index.js
@@ -20,10 +20,7 @@ const orderRoute = require("./routes/Order");
 app.use(express.json())
 
 app.use(cors({
-    origin: "*", // La URL exacta de tu frontend
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"]
+    origin: "https://proyecto-productivo-sena-msv3.vercel.app", // La URL exacta de tu frontend
 }));
 
 
